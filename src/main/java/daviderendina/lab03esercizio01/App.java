31f4a7bedaf9a376df94a98efef20acc5b023729
Davide Rendina <daviderendina9@gmail.com>
2019-10-29 11:54:18 +0100

    Caricamento esercizio 1

---
+++ src/main/java/daviderendina/lab03esercizio01/App.java
@@ -0,0 +1,17 @@
+package daviderendina.lab03esercizio01;
+
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println( "Hello World!" );
+    }
+}
